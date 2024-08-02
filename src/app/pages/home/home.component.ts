77fdec36f8675c6f57d2bb4e147dbfd316a92e4f
leop <leo.petit@raccourci.fr>
2024-08-02 14:11:50 +0200

    Feature: update service and print data on home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { Olympic } from '../../core/models/Olympic';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,15 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
+  olympicData: Olympic[] = [];
 
-  constructor(private olympicService: OlympicService) {}
+  constructor(private olympicService: OlympicService) {
+
+  }
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
-  }
+    this.olympicService.loadInitialData().subscribe();
+    this.olympicService.getOlympics().subscribe((data: Olympic[]) => {
+      this.olympicData = data;
+    });  }
 }
